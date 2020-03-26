7a8d8d937a1844a11ea7054f43c57280cad2c975
tiagoosr <tiiago.santosr@gmail.com>
2020-03-26 15:46:58 -0300

    make it better

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const app = require('../app');
 const mongoose = require('mongoose');
 
 require('dotenv').config({ path: 'variables.env' })
-const URI = process.env.DATABASE
+const URI = process.env.MONGO_URL
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise; //posso usar promessas
 mongoose.connection.on('error', (error) => {
     console.error("ERRO: " + error.message)
 });
 
-app.set('port', process.env.PORT || '0.0.0.0');
+app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
     console.log("servidor rodando na porta: " + server.address().port);
 });
